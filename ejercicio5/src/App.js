be2b1e349649e25292c19585e72a700f337a23c4
Diego Vistoso <diegoivg98@gmail.com>
2022-12-07 00:18:49 -0300

    Add files via upload
    agregar ejercicio formik yup

---
+++ ejercicio5/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskFormik from './components/taskFormik';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskFormik></TaskFormik>
+    </div>
+  );
+}
+
+export default App;
